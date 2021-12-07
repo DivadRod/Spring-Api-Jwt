ff71114c3ebadb3296f898cdcac01839544884bc
David R <davrodzam@gmail.com>
2021-12-06 19:01:40 -0600

    inicio

---
+++ src/test/java/com/mutual/SpringApiJwt/SpringApiJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutual.SpringApiJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
