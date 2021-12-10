86b3fca8b8079aedbc2ab06e6f242463e9dc1263
David R <davrodzam@gmail.com>
2021-12-10 15:46:02 -0600

    Query Methods

--- src/main/java/com/mutual/SpringApiJwt/persistence/ProductoRepository.java
+++ src/main/java/com/mutual/SpringApiJwt/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.mutual.SpringApiJwt.persistence.crud.ProductoCrudRepository;
 import com.mutual.SpringApiJwt.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
@@ -11,4 +12,22 @@ public class ProductoRepository {
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
     }
+
+
+    /**
+     * Query Methods
+     */
+    public List<Producto>getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<List<Producto>> getProductoByPrecioVentaLessThanPriceAndByCategory (int precioVenta, int idCategoria) {
+        return productoCrudRepository.findByPrecioVentaLessThanAndIdCategoriaOrderByNombreAsc(precioVenta, idCategoria);
+    }
+
+
 }
--- src/main/java/com/mutual/SpringApiJwt/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/mutual/SpringApiJwt/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,11 @@
 package com.mutual.SpringApiJwt.persistence.crud;
 
 import com.mutual.SpringApiJwt.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Extiende de la libreria
@@ -22,5 +26,19 @@ import org.springframework.data.repository.CrudRepository;
  * */
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    //Convencional
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+
+    //Forma nativa
+    List<Producto> findByIdCategoria(int idCategoria);
+
+    //Forma nativa
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+    //Reto1
+    Optional<List<Producto>> findByPrecioVentaLessThanAndIdCategoriaOrderByNombreAsc(int precioVenta, int idCategoria);
 
 }
