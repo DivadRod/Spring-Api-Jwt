224a8dca4a2b27c23139f2e1ab4e6931864a9fdf
David R <davrodzam@gmail.com>
2021-12-10 16:18:35 -0600

    Repository

--- src/main/java/com/mutual/SpringApiJwt/persistence/ProductoRepository.java
+++ src/main/java/com/mutual/SpringApiJwt/persistence/ProductoRepository.java
@@ -2,10 +2,16 @@ package com.mutual.SpringApiJwt.persistence;
 
 import com.mutual.SpringApiJwt.persistence.crud.ProductoCrudRepository;
 import com.mutual.SpringApiJwt.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * @Repository
+ * Esta es la clase que interactua con la DB
+ * */
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -25,9 +31,20 @@ public class ProductoRepository {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 
+    //Reto#1
     public Optional<List<Producto>> getProductoByPrecioVentaLessThanPriceAndByCategory (int precioVenta, int idCategoria) {
         return productoCrudRepository.findByPrecioVentaLessThanAndIdCategoriaOrderByNombreAsc(precioVenta, idCategoria);
     }
 
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
 
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
 }
