6fe94963dad12c7cad1663328cda5dc76efbb356
David R <davrodzam@gmail.com>
2021-12-10 20:30:25 -0600

    Data mapper pattern

---
+++ src/main/java/com/mutual/SpringApiJwt/domain/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.mutual.SpringApiJwt.domain.repository;
+
+import com.mutual.SpringApiJwt.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> getAll();
+    Optional<List<Product>> getByCategory(int categoryId);
+    Optional<List<Product>> getScarseProducts(int quantity);
+    Optional<Product> getProduct(int productId);
+    Product save(Product product);
+    void delete(int productId);
+}
